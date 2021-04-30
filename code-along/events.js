18a31fd5d6fab3740ddac099ecae3df077c690e0
Josh Rosenthal <josh.rosenthal@kellogg.northwestern.edu>
2021-04-29 20:48:50 -0500

    Done

--- code-along/events.js
+++ code-along/events.js
@@ -1,19 +1,73 @@
-// Get a reference to the page title element from the DOM
-let pageTitleElement = document.querySelector(`.page-title`)
+// // Get a reference to the page title element from the DOM
+// let pageTitleElement = document.querySelector(`.page-title`)
 
-// Modify the page title element's contents
-pageTitleElement.innerHTML = `Movies to watch!`
+// // Modify the page title element's contents
+// pageTitleElement.innerHTML = `Movies to watch!`
 
-// Get a reference to the bulleted list of movies
-let movieList = document.querySelector(`.movies-to-watch`)
+// // Get a reference to the bulleted list of movies
+// let movieList = document.querySelector(`.movies-to-watch`)
 
-// Add an new list item to the bulleted list of movies
-movieList.insertAdjacentHTML(`beforeend`, `
-  <li>Spaceballs</li>
-`)
+// // Add an new list item to the bulleted list of movies
+// movieList.insertAdjacentHTML(`beforeend`, `
+//   <li>Spaceballs</li>
+// `)
 
-// Get a reference to the image
+// // Get a reference to the image
+// let image = document.querySelector(`img`)
+
+// // Add the `border-pink-500` class to the image element
+// image.classList.add(`border-pink-500`)
+
+// Get a reerence to the "Click me" button
+let clickMeButton = document.querySelector(`.click-me`)
+
+// // Event listener for the "click me"nuttom
+// clickMeButton.addEventListener(`click`, async function(event) {
+// // show an alert
+//    let pageTitleElement = document.querySelector(`.page-title`)
+
+//    pageTitleElement.innerHTML = `Movies to Watch!`
+
+//    let addMovieLink = document.querySelector(`.add-movie`)
+
+//    addMovieLink.addEventListener(`click`, asyn function(event) {
+//     event.preventDefault()
+
+   
+
+// movieList.insertAdjacentHTML(`beforeend`, `
+//   <li>Spaceballs</li>
+// `)
+
+//    })
+
+   
+// })
+
+let zoomImageButton = document.querySelector(`.zoom-image`)
+
+zoomImageButton.addEventListener(`click`, async function(event) {
+
+// // Get a reference to the image
 let image = document.querySelector(`img`)
 
-// Add the `border-pink-500` class to the image element
-image.classList.add(`border-pink-500`)
+// // Add the `border-pink-500` class to the image element
+// image.classList.add(`border-pink-500`)
+
+image.classList.toggle(`w-96`)
+image.classList.toggle(`w-Full`)
+
+})
+
+//
+
+let changeImageButton = document.querySelector(`.change-image`)
+
+//
+
+changeImageButton.addEventListener(`click`, async function(event){
+  let image = document.querySelector(`img`)
+
+  image.setAttribute(`src`, `../images/grogu2.jpg`)
+
+})
--- lab/week5.js
+++ lab/week5.js
@@ -15,26 +15,43 @@
 
 window.addEventListener('DOMContentLoaded', async function() {
   // Get a reference to the "get weather" button
-
+  let weatherButton = document.querySelector(`.get-weather`)
   // When the "get weather" button is clicked:
-
+  weatherButton.addEventListener(`click`, async function(event) {
     // - Ignore the default behavior of the button
-
+    event.preventDefault()
     // - Get a reference to the element containing the user-entered location
-
+    let location = document.querySelector(`#location`)
     // - Get the user-entered location from the element's value
-
+    let definedLocation = location.value
     // - Check to see if the user entered anything; if so:
-
+    if (definedLocation.length > 0) {
+    
       // - Construct a URL to call the WeatherAPI.com API
-
+      let url = `https://api.weatherapi.com/v1/forecast.json?key=f1e45285c93842b4bce11822213004&q=${definedLocation}&days=3`
       // - Fetch the url, wait for a response, store the response in memory
-
+      let response = await fetch(url)
       // - Ask for the json-formatted data from the response, wait for the data, store it in memory
-
+      let json = await response.json()
       // - Write the json-formatted data to the JavaScript console
-
+      console.log(json)
       // - Store the interpreted location, current weather conditions, the forecast as three separate variables
+      let place = json.location
+      let currentWeather = json.current
+      let forecast = json.forecast
 
+      let currentElement = document.querySelector(`.current`)
+      currentElement.innerHTML = `
+      <div class="text-center space-y-2">
+        <div class="font-bold text-3xl">Current Weather for ${place.name}, ${place.region}</div>
+        <div class="font-bold">
+          <img src="https:${currentWeather.condition.icon}" class="inline-block">
+          <span class="temperature">${currentWeather.temp_f}</span>° 
+          and
+          <span class="conditions">${currentWeather.condition.text}</span>
+        </div>
+      </div>`
       // - Continue the recipe yourself!
+    }
+  })
 })
